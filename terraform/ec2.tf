093e63e5269943bd9daff5e3057dbec9c80eb284
abhijit0903 <abhijit.s.gawande@gmail.com>
2021-10-12 15:26:59 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,1 +1,28 @@
-abcd
+provider "aws" {
+     region = "us-east-1"
+}
+
+data "aws_ami" "packerDemo" {
+     most_recent = true
+
+     filter {
+        name   = "name"
+        values = ["PackerDemo*"]
+
+ }
+     filter {
+       name   = "virtualization-type"
+       values = ["hvm"]
+ }
+     owners = ["****"] # Canonical
+ }
+
+resource "aws_instance" "example" {
+     ami = data.aws_ami.packerDemo.id
+     instance_type = "t2.micro"
+     availability_zone = var.availability_zone
+
+lifecycle {
+     ignore_changes = [ami]
+     }
+ }
